a3092412ac8f638292dd8e59323e46d544661106
sweetanzell <sweetanzell@gmail.com>
2013-06-05 16:03:05 +0545

    Theming

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,19 @@
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'waterfall'
-        },
+$(".circle1").hover(
+  function () {
+    $(this).siblings(".year1").css('display','block');
+     $(this).siblings(".percent1").css('display','block');
+  },
+  function () {
+    $(this).siblings(".year1").css('display','none');
+     $(this).siblings(".percent1").css('display','none');
+  }
+);
 
-        title: {
-            text: 'Highcharts Waterfall'
-        },
-
-        xAxis: {
-            type: 'category'
-        },
-
-        yAxis: {
-            title: {
-                text: 'USD'
-            }
-        },
-
-        legend: {
-            enabled: false
-        },
-
-        tooltip: {
-            pointFormat: '<b>${point.y:,.2f}</b> USD'
-        },
-
-        series: [{
-            upColor: Highcharts.getOptions().colors[5],
-            color: Highcharts.getOptions().colors[2],
-            data: [{
-                name: '2010',
-                y: 1035282
-            }, {
-                name: '2011',
-                y: 3111858
-            }, {
-                name: '2012',
-                y: 4867254
-            }, {
-                name: '2013',
-                 y: 4867254,
-                // isIntermediateSum: true,
-                // color: Highcharts.getOptions().colors[1]
-            }],
-            dataLabels: {
-                enabled: true,
-                formatter: function () {
-                    return Highcharts.numberFormat(this.y / 1000, 0, ',') + 'k';
-                },
-                style: {
-                    color: '#FFFFFF',
-                    fontWeight: 'bold'
-                }
-            },
-            pointPadding: 0
-        }]
-    });
-});
-
+// $(".circle1").hover(
+//   function () {
+//     $(this).siblings(".percent1").css('display','block');
+//   },
+//   function () {
+//     $(this).siblings(".percent2").css('display','none');
+//   }
+// );
